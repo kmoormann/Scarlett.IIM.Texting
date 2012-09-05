664ffceadbeca5b26a81cf1259aacaea4f4e485c
Kevin Moormann <kevin.moormann@parivedasolutions.com>
2012-09-04 22:54:39 -0500

    initial commit

---
+++ Scarlett.IIM.Twilio.MVC4/Controllers/ResponseController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio.TwiML;
+using Twilio.TwiML.Mvc;
+
+namespace Scarlett.IIM.Twilio.MVC4.Controllers
+{
+    public class ResponseController : Controller
+    {
+        //
+        // GET: /Response/
+        [HttpGet]
+        [HttpPost]
+        public ActionResult Index()
+        {
+            ActionResult result = null;
+            switch (HttpContext.Request.HttpMethod)
+            {
+                case "GET":
+                    result = IndexGet();
+                    break;
+                case "POST":
+                    result = IndexPost();
+                    break;
+                default:
+                    result = null;
+                    break;
+            }
+            return result;
+        }
+
+        private ActionResult IndexPost()
+        {
+            var response = new TwilioResponse();
+            response.Say("this is my response");
+            return new TwiMLResult(response);
+        }
+
+        private ActionResult IndexGet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
